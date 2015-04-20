fc40567c66e57f48f4162e4033e5712c55491e8f
Krystian Dużyński <krystian.duzynski@gmail.com>
2015-04-20 19:30:16 +0200

    added support for bool and int types

--- src/gen.cpp
+++ src/gen.cpp
@@ -8,7 +8,6 @@ mp_obj_t hSystem_delay_ms(mp_obj_t self_in, mp_obj_t arg0) {
 	hSystem *hSelf = (hSystem*)self->hObj;
 	mp_int_t val0 = mp_obj_get_int(arg0);
 	hSelf->delay_ms(val0);
-
 	return mp_const_none;
 }
 mp_obj_t hSystem_delay_us(mp_obj_t self_in, mp_obj_t arg0) {
@@ -16,7 +15,24 @@ mp_obj_t hSystem_delay_us(mp_obj_t self_in, mp_obj_t arg0) {
 	hSystem *hSelf = (hSystem*)self->hObj;
 	mp_int_t val0 = mp_obj_get_int(arg0);
 	hSelf->delay_us(val0);
-
+	return mp_const_none;
+}
+mp_obj_t hLED_on(mp_obj_t self_in) {
+	mp_obj_hLED_t *self = (mp_obj_hLED_t*)self_in;
+	hLED *hSelf = (hLED*)self->hObj;
+	hSelf->on();
+	return mp_const_none;
+}
+mp_obj_t hLED_off(mp_obj_t self_in) {
+	mp_obj_hLED_t *self = (mp_obj_hLED_t*)self_in;
+	hLED *hSelf = (hLED*)self->hObj;
+	hSelf->off();
+	return mp_const_none;
+}
+mp_obj_t hLED_toggle(mp_obj_t self_in) {
+	mp_obj_hLED_t *self = (mp_obj_hLED_t*)self_in;
+	hLED *hSelf = (hLED*)self->hObj;
+	hSelf->toggle();
 	return mp_const_none;
 }
 mp_obj_t hMotor_setPower(mp_obj_t self_in, mp_obj_t arg0) {
@@ -24,7 +40,6 @@ mp_obj_t hMotor_setPower(mp_obj_t self_in, mp_obj_t arg0) {
 	hMotor *hSelf = (hMotor*)self->hObj;
 	mp_int_t val0 = mp_obj_get_int(arg0);
 	hSelf->setPower(val0);
-
 	return mp_const_none;
 }
 mp_obj_t hMotor_rotAbs(mp_obj_t self_in, mp_obj_t arg0) {
@@ -32,27 +47,60 @@ mp_obj_t hMotor_rotAbs(mp_obj_t self_in, mp_obj_t arg0) {
 	hMotor *hSelf = (hMotor*)self->hObj;
 	mp_int_t val0 = mp_obj_get_int(arg0);
 	hSelf->rotAbs(val0);
-
 	return mp_const_none;
 }
 mp_obj_t hMotor_setParallelMode(mp_obj_t self_in) {
 	mp_obj_hMotor_t *self = (mp_obj_hMotor_t*)self_in;
 	hMotor *hSelf = (hMotor*)self->hObj;
 	hSelf->setParallelMode();
-
 	return mp_const_none;
 }
 mp_obj_t hMotor_setSingleMode(mp_obj_t self_in) {
 	mp_obj_hMotor_t *self = (mp_obj_hMotor_t*)self_in;
 	hMotor *hSelf = (hMotor*)self->hObj;
 	hSelf->setSingleMode();
-
 	return mp_const_none;
+}
+mp_obj_t hMotor_resetEncoderCnt(mp_obj_t self_in) {
+	mp_obj_hMotor_t *self = (mp_obj_hMotor_t*)self_in;
+	hMotor *hSelf = (hMotor*)self->hObj;
+	hSelf->resetEncoderCnt();
+	return mp_const_none;
+}
+mp_obj_t hMotor_getEncoderCnt(mp_obj_t self_in) {
+	mp_obj_hMotor_t *self = (mp_obj_hMotor_t*)self_in;
+	hMotor *hSelf = (hMotor*)self->hObj;
+	int ret = hSelf->getEncoderCnt();
+	return mp_obj_new_int(ret);
+}
+mp_obj_t hButton_isPressed(mp_obj_t self_in) {
+	mp_obj_hButton_t *self = (mp_obj_hButton_t*)self_in;
+	hButton *hSelf = (hButton*)self->hObj;
+	bool ret = hSelf->isPressed();
+	return ret ? mp_const_true : mp_const_false;
+}
+mp_obj_t hButton_waitForPressed(mp_obj_t self_in, mp_obj_t arg0) {
+	mp_obj_hButton_t *self = (mp_obj_hButton_t*)self_in;
+	hButton *hSelf = (hButton*)self->hObj;
+	mp_int_t val0 = mp_obj_get_int(arg0);
+	bool ret = hSelf->waitForPressed(val0);
+	return ret ? mp_const_true : mp_const_false;
+}
+mp_obj_t hButton_waitForReleased(mp_obj_t self_in, mp_obj_t arg0) {
+	mp_obj_hButton_t *self = (mp_obj_hButton_t*)self_in;
+	hButton *hSelf = (hButton*)self->hObj;
+	mp_int_t val0 = mp_obj_get_int(arg0);
+	bool ret = hSelf->waitForReleased(val0);
+	return ret ? mp_const_true : mp_const_false;
 }
 
 extern const mp_obj_type_t hSystem_type;
 
+extern const mp_obj_type_t hLED_type;
+
 extern const mp_obj_type_t hMotor_type;
+
+extern const mp_obj_type_t hButton_type;
 
 void reg()
 {
@@ -63,6 +111,27 @@ void reg()
 		v->hObj = &sys;
 		v->base.type = &hSystem_type;
 		mp_store_name(MP_QSTR_sys, (mp_obj_t)v);
+	}
+
+	{
+		mp_obj_hLED_t *v = m_new_obj_var(mp_obj_hLED_t, char*, 0);
+		v->hObj = &LED1;
+		v->base.type = &hLED_type;
+		mp_store_name(MP_QSTR_LED1, (mp_obj_t)v);
+	}
+
+	{
+		mp_obj_hLED_t *v = m_new_obj_var(mp_obj_hLED_t, char*, 0);
+		v->hObj = &LED2;
+		v->base.type = &hLED_type;
+		mp_store_name(MP_QSTR_LED2, (mp_obj_t)v);
+	}
+
+	{
+		mp_obj_hLED_t *v = m_new_obj_var(mp_obj_hLED_t, char*, 0);
+		v->hObj = &LED3;
+		v->base.type = &hLED_type;
+		mp_store_name(MP_QSTR_LED3, (mp_obj_t)v);
 	}
 
 	{
@@ -105,6 +174,20 @@ void reg()
 		v->hObj = &hMot6;
 		v->base.type = &hMotor_type;
 		mp_store_name(MP_QSTR_hMot6, (mp_obj_t)v);
+	}
+
+	{
+		mp_obj_hButton_t *v = m_new_obj_var(mp_obj_hButton_t, char*, 0);
+		v->hObj = &hBtn1;
+		v->base.type = &hButton_type;
+		mp_store_name(MP_QSTR_hBtn1, (mp_obj_t)v);
+	}
+
+	{
+		mp_obj_hButton_t *v = m_new_obj_var(mp_obj_hButton_t, char*, 0);
+		v->hObj = &hBtn2;
+		v->base.type = &hButton_type;
+		mp_store_name(MP_QSTR_hBtn2, (mp_obj_t)v);
 	}
 
 }
--- src/gen.h
+++ src/gen.h
@@ -20,20 +20,35 @@ extern const qstr_pool_t hpyframework_pool;
 enum
 {
 	start = MP_QSTR_number_of - 1,
+	MP_QSTR_LED1,
+	MP_QSTR_LED2,
+	MP_QSTR_LED3,
 	MP_QSTR_hMot1,
 	MP_QSTR_hMot2,
 	MP_QSTR_hMot3,
 	MP_QSTR_hMot4,
 	MP_QSTR_hMot5,
 	MP_QSTR_hMot6,
+	MP_QSTR_hBtn1,
+	MP_QSTR_hBtn2,
 	MP_QSTR_hSystem,
 	MP_QSTR_delay_ms,
 	MP_QSTR_delay_us,
+	MP_QSTR_hLED,
+	MP_QSTR_on,
+	MP_QSTR_off,
+	MP_QSTR_toggle,
 	MP_QSTR_hMotor,
 	MP_QSTR_setPower,
 	MP_QSTR_rotAbs,
 	MP_QSTR_setParallelMode,
 	MP_QSTR_setSingleMode,
+	MP_QSTR_resetEncoderCnt,
+	MP_QSTR_getEncoderCnt,
+	MP_QSTR_hButton,
+	MP_QSTR_isPressed,
+	MP_QSTR_waitForPressed,
+	MP_QSTR_waitForReleased,
 	MP_QSTR_hPyFramework_number_of,
 };
 
@@ -42,10 +57,18 @@ extern "C" {
 #endif
 mp_obj_t hSystem_delay_ms(mp_obj_t self_in, mp_obj_t arg0);
 mp_obj_t hSystem_delay_us(mp_obj_t self_in, mp_obj_t arg0);
+mp_obj_t hLED_on(mp_obj_t self_in);
+mp_obj_t hLED_off(mp_obj_t self_in);
+mp_obj_t hLED_toggle(mp_obj_t self_in);
 mp_obj_t hMotor_setPower(mp_obj_t self_in, mp_obj_t arg0);
 mp_obj_t hMotor_rotAbs(mp_obj_t self_in, mp_obj_t arg0);
 mp_obj_t hMotor_setParallelMode(mp_obj_t self_in);
 mp_obj_t hMotor_setSingleMode(mp_obj_t self_in);
+mp_obj_t hMotor_resetEncoderCnt(mp_obj_t self_in);
+mp_obj_t hMotor_getEncoderCnt(mp_obj_t self_in);
+mp_obj_t hButton_isPressed(mp_obj_t self_in);
+mp_obj_t hButton_waitForPressed(mp_obj_t self_in, mp_obj_t arg0);
+mp_obj_t hButton_waitForReleased(mp_obj_t self_in, mp_obj_t arg0);
 
 #ifdef __cplusplus
 }
@@ -57,8 +80,20 @@ typedef struct _mp_obj_hSystem_t
 	void *hObj;
 } mp_obj_hSystem_t;
 
+typedef struct _mp_obj_hLED_t
+{
+	mp_obj_base_t base;
+	void *hObj;
+} mp_obj_hLED_t;
+
 typedef struct _mp_obj_hMotor_t
 {
 	mp_obj_base_t base;
 	void *hObj;
 } mp_obj_hMotor_t;
+
+typedef struct _mp_obj_hButton_t
+{
+	mp_obj_base_t base;
+	void *hObj;
+} mp_obj_hButton_t;
