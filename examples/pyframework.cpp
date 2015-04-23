73b71d4b7f77b840350b359e2e0eaf9b881f99e1
Krystian Dużyński <krystian.duzynski@gmail.com>
2015-04-23 14:10:13 +0200

    better repl, ignoring special keys

--- examples/pyframework.cpp
+++ examples/pyframework.cpp
@@ -48,17 +48,22 @@ void qqq()
 		LED2.toggle();
 		sys.delay_ms(50);
 	}
-	// hPython::eval("print('aaaa',2+4*9)");
+	hPython::eval("hSens1.selectI2C()");
+	hPython::eval("a=hSens1.getI2C()");
+	hPython::eval("b=[0x42]");
+	hPython::eval("c=[0]");
 	
 	Serial.printf("> ");
 	
 	char buff[2048];
 	unsigned int i = 0;
+	int specialState = 0;
 	while (1)
 	{
 		char ch;
 		queue.receive(ch);
-		Serial.printf("%c", ch);
+		// Serial.printf("0x%02x\r\n", ch);
+		// continue;
 		if (ch == 0x7f)
 		{
 			if (i > 0)
@@ -68,6 +73,29 @@ void qqq()
 			}
 			continue;
 		}
+		if (ch == 0x1b || ch == 0x3b)
+		{
+			specialState = 1;
+			continue;
+		}
+		if (specialState == 1)
+		{
+			if (ch == 0x1b)
+			{
+				specialState = 1;
+			}
+			else
+			{
+				specialState = 2;
+			}
+			continue;
+		}
+		if (specialState == 2)
+		{
+			specialState = 0;
+			continue;
+		}
+		Serial.printf("%c", ch);
 		if (i >= sizeof(buff))
 			i = 0;
 		if (ch == '\r')
@@ -91,14 +119,14 @@ void qqq()
 				i = 0;
 				hPython::eval(buff);
 				Serial.printf("> ");
-				LED2.toggle();
+				// LED2.toggle();
 			}
 		}
 		else
 		{
 			i++;
 		}
-		LED1.toggle();
+		// LED1.toggle();
 	}
 }
 
